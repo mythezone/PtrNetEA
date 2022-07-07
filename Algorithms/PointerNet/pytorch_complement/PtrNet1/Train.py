8b7daed28a765912926b53531999852353e0104a
Mythezone <125616515@qq.com>
2022-07-07 16:45:08 +0800

    fix bugs

--- Algorithms/PointerNet/pytorch_complement/PtrNet1/Train.py
+++ Algorithms/PointerNet/pytorch_complement/PtrNet1/Train.py
@@ -19,95 +19,98 @@ from tqdm import tqdm
 from PointerNet import PointerNet
 from Data_Generator import TSPDataset
 
-parser = argparse.ArgumentParser(description="Pytorch implementation of Pointer-Net")
 
-# Data
-# parser.add_argument('--train_size', default=1000000, type=int, help='Training data size')
-# parser.add_argument('--val_size', default=10000, type=int, help='Validation data size')
-# parser.add_argument('--test_size', default=10000, type=int, help='Test data size')
-# parser.add_argument('--batch_size', default=256, type=int, help='Batch size')
-# Train
-parser.add_argument('--nof_epoch', default=50000, type=int, help='Number of epochs')
-parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
-# GPU
-parser.add_argument('--gpu', default=False, action='store_true', help='Enable gpu')
-# TSP
-parser.add_argument('--nof_points', type=int, default=5, help='Number of points in TSP')
-# Network
-parser.add_argument('--embedding_size', type=int, default=128, help='Embedding size')
-parser.add_argument('--hiddens', type=int, default=512, help='Number of hidden units')
-parser.add_argument('--nof_lstms', type=int, default=2, help='Number of LSTM layers')
-parser.add_argument('--dropout', type=float, default=0., help='Dropout value')
-parser.add_argument('--bidir', default=True, action='store_true', help='Bidirectional')
+if __name__ == '__main__':
+        
+    parser = argparse.ArgumentParser(description="Pytorch implementation of Pointer-Net")
 
-# for Test
-parser.add_argument('--train_size', default=2000, type=int, help='Training data size')
-parser.add_argument('--val_size', default=500, type=int, help='Validation data size')
-parser.add_argument('--test_size', default=500, type=int, help='Test data size')
-parser.add_argument('--batch_size', default=256, type=int, help='Batch size')
+    # Data
+    parser.add_argument('--train_size', default=1000000, type=int, help='Training data size')
+    parser.add_argument('--val_size', default=10000, type=int, help='Validation data size')
+    parser.add_argument('--test_size', default=10000, type=int, help='Test data size')
+    parser.add_argument('--batch_size', default=256, type=int, help='Batch size')
+    # Train
+    parser.add_argument('--nof_epoch', default=50000, type=int, help='Number of epochs')
+    parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
+    # GPU
+    parser.add_argument('--gpu', default=False, action='store_true', help='Enable gpu')
+    # TSP
+    parser.add_argument('--nof_points', type=int, default=5, help='Number of points in TSP')
+    # Network
+    parser.add_argument('--embedding_size', type=int, default=128, help='Embedding size')
+    parser.add_argument('--hiddens', type=int, default=512, help='Number of hidden units')
+    parser.add_argument('--nof_lstms', type=int, default=2, help='Number of LSTM layers')
+    parser.add_argument('--dropout', type=float, default=0., help='Dropout value')
+    parser.add_argument('--bidir', default=True, action='store_true', help='Bidirectional')
 
-params = parser.parse_args()
+    # for Test
+    # parser.add_argument('--train_size', default=2000, type=int, help='Training data size')
+    # parser.add_argument('--val_size', default=500, type=int, help='Validation data size')
+    # parser.add_argument('--test_size', default=500, type=int, help='Test data size')
+    # parser.add_argument('--batch_size', default=256, type=int, help='Batch size')
 
-if params.gpu and torch.cuda.is_available():
-    USE_CUDA = True
-    print('Using GPU, %i devices.' % torch.cuda.device_count())
-else:
-    USE_CUDA = False
+    params = parser.parse_args()
 
-model = PointerNet(params.embedding_size,
-                   params.hiddens,
-                   params.nof_lstms,
-                   params.dropout,
-                   params.bidir)
+    if params.gpu and torch.cuda.is_available():
+        USE_CUDA = True
+        print('Using GPU, %i devices.' % torch.cuda.device_count())
+    else:
+        USE_CUDA = False
 
-dataset = TSPDataset(params.train_size,
-                     params.nof_points)
+    model = PointerNet(params.embedding_size,
+                    params.hiddens,
+                    params.nof_lstms,
+                    params.dropout,
+                    params.bidir)
 
-dataloader = DataLoader(dataset,
-                        batch_size=params.batch_size,
-                        shuffle=True,
-                        num_workers=4)
+    dataset = TSPDataset(params.train_size,
+                        params.nof_points)
 
-if USE_CUDA:
-    model.cuda()
-    net = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
-    cudnn.benchmark = True
+    dataloader = DataLoader(dataset,
+                            batch_size=params.batch_size,
+                            shuffle=True,
+                            num_workers=4)
 
-CCE = torch.nn.CrossEntropyLoss()
-model_optim = optim.Adam(filter(lambda p: p.requires_grad,
-                                model.parameters()),
-                         lr=params.lr)
+    if USE_CUDA:
+        model.cuda()
+        net = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
+        cudnn.benchmark = True
 
-losses = []
+    CCE = torch.nn.CrossEntropyLoss()
+    model_optim = optim.Adam(filter(lambda p: p.requires_grad,
+                                    model.parameters()),
+                            lr=params.lr)
 
-for epoch in range(params.nof_epoch):
-    batch_loss = []
-    iterator = tqdm(dataloader, unit='Batch')
+    losses = []
 
-    for i_batch, sample_batched in enumerate(iterator):
-        iterator.set_description('Batch %i/%i' % (epoch+1, params.nof_epoch))
+    for epoch in range(params.nof_epoch):
+        batch_loss = []
+        iterator = tqdm(dataloader, unit='Batch')
 
-        train_batch = Variable(sample_batched['Points'])
-        target_batch = Variable(sample_batched['Solution'])
+        for i_batch, sample_batched in enumerate(iterator):
+            iterator.set_description('Batch %i/%i' % (epoch+1, params.nof_epoch))
 
-        if USE_CUDA:
-            train_batch = train_batch.cuda()
-            target_batch = target_batch.cuda()
+            train_batch = Variable(sample_batched['Points'])
+            target_batch = Variable(sample_batched['Solution'])
 
-        o, p = model(train_batch)
-        o = o.contiguous().view(-1, o.size()[-1])
+            if USE_CUDA:
+                train_batch = train_batch.cuda()
+                target_batch = target_batch.cuda()
 
-        target_batch = target_batch.view(-1)
+            o, p = model(train_batch)
+            o = o.contiguous().view(-1, o.size()[-1])
 
-        loss = CCE(o, target_batch)
+            target_batch = target_batch.view(-1)
 
-        losses.append(loss.data)
-        batch_loss.append(loss.data)
+            loss = CCE(o, target_batch)
 
-        model_optim.zero_grad()
-        loss.backward()
-        model_optim.step()
+            losses.append(loss.data)
+            batch_loss.append(loss.data)
 
-        iterator.set_postfix(loss='{}'.format(loss.data))
+            model_optim.zero_grad()
+            loss.backward()
+            model_optim.step()
 
-    iterator.set_postfix(loss=np.average(batch_loss))
+            iterator.set_postfix(loss='{}'.format(loss.data))
+
+        iterator.set_postfix(loss=np.average(batch_loss))
